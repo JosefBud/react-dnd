d966e98d1a0bbba0593e9b498225c8ca20758772
JosefBud <josefbud@gmail.com>
2019-08-01 19:14:43 -0400

    Setting up background image

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -23,13 +23,13 @@ class App extends Component {
   }
   render() {
     return (
-      <Container>
+      <Container fluid>
+        <img src="media/dragon.svg" className="background-image" />
         <Row>
           <Col>
             <h1>{this.state.test}</h1>
             <Button onClick={this.handleClick}>Change me, daddy</Button>
             <h1>It works right now though</h1>
-            <img src="media/dragon.svg" />
           </Col>
         </Row>
       </Container>
