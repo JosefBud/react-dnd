13495ef8b1377eefb96b6afae4ea0c21e6ef3b1f
JosefBud <josefbud@gmail.com>
2019-08-02 23:12:10 -0400

    Addition of stats and beginning of skills

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -9,7 +9,11 @@ import {
   InputGroup,
   FormControl,
   Dropdown,
-  DropdownButton
+  DropdownButton,
+  Card,
+  CardGroup,
+  Badge,
+  ListGroup
 } from "react-bootstrap";
 import Test from "./components/test";
 import "./index.html";
@@ -24,6 +28,56 @@ class App extends Component {
       characterXP: 0,
       addXP: 0,
       characterLevel: 1,
+      stats: {
+        strength: 0,
+        strengthMod: 0,
+        dexterity: 0,
+        dexterityMod: 0,
+        constitution: 0,
+        constitutionMod: 0,
+        intelligence: 0,
+        intelligenceMod: 0,
+        wisdom: 0,
+        wisdomMod: 0,
+        charisma: 0,
+        charismaMod: 0
+      },
+      skills: {
+        acrobaticsProf: false,
+        acrobaticsExp: false,
+        animalHandlingProf: false,
+        animalHandlingExp: false,
+        arcanaProf: false,
+        arcanaExp: false,
+        deceptionProf: false,
+        deceptionExp: false,
+        historyProf: false,
+        historyExp: false,
+        insightProf: false,
+        insightExp: false,
+        intimidationProf: false,
+        intimidationExp: false,
+        investigationProf: false,
+        investigationExp: false,
+        medicineProf: false,
+        medicineExp: false,
+        natureProf: false,
+        natureExp: false,
+        perceptionProf: false,
+        perceptionExp: false,
+        performanceProf: false,
+        performanceExp: false,
+        persuasionProf: false,
+        persuasionExp: false,
+        religionProf: false,
+        religionExp: false,
+        sleightOfHandProf: false,
+        sleightOfHandExp: false,
+        stealthProf: false,
+        stealthExp: false,
+        survivalProf: false,
+        survivalExp: false
+      },
       inputFields: [1, 2, 3, 4],
       test: "oh hey"
     };
@@ -31,7 +85,29 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.renderField = this.renderField.bind(this);
     this.handleXPAdd = this.handleXPAdd.bind(this);
+    this.calculateStatModifier = this.calculateStatModifier.bind(this);
+    this.calculateSkillModifier = this.calculateSkillModifier.bind(this);
+    this.renderStatCards = this.renderStatCards.bind(this);
+    this.renderSkillCards = this.renderSkillCards.bind(this);
   }
+  componentDidUpdate() {}
+
+  calculateStatModifier(statModName, points) {
+    let parsedPoints = parseInt(points);
+    let modifier = Math.round((parsedPoints - 11) / 2);
+    if (modifier < 0) {
+      modifier = 0;
+    }
+
+    this.setState(prevState => ({
+      stats: {
+        ...prevState.stats,
+        [statModName]: modifier
+      }
+    }));
+  }
+
+  calculateSkillModifier(skill) {}
 
   handleClick() {
     let inputFields = this.state.inputFields;
@@ -54,6 +130,92 @@ class App extends Component {
         </div>
       );
     });
+  }
+
+  renderStatCards(statName) {
+    const statVarName = statName.toLowerCase();
+    const statModName = statVarName + "Mod";
+
+    return (
+      <Card>
+        <Card.Title>{statName}</Card.Title>
+        <h2>+{this.state.stats[statModName]}</h2>
+        <InputGroup className="input--smaller mx-auto">
+          <FormControl
+            value={this.state.stats[statVarName]}
+            className="input--stats"
+            aria-label="input"
+            aria-describedby={statName}
+            onChange={event => {
+              this.setState({
+                stats: {
+                  ...this.state.stats,
+                  [statVarName]: event.target.value
+                }
+              });
+              this.calculateStatModifier(statModName, event.target.value);
+            }}
+          />
+        </InputGroup>
+      </Card>
+    );
+  }
+
+  renderSkillCards(skillName, relatedStat) {
+    const skillProf = skillName.toLowerCase() + "Prof";
+    const skillExp = skillName.toLowerCase() + "Exp";
+    return (
+      <ListGroup.Item className="py-0">
+        <Row className="py-0">
+          <Col lg="2" className="text-center px-0">
+            <p>Prof.</p>
+          </Col>
+          <Col lg="2" className="text-center px-0">
+            <p>Exp.</p>
+          </Col>
+          <Col lg="8 text-right">
+            {skillName} <Badge variant="secondary">{relatedStat}</Badge>
+          </Col>
+        </Row>
+        <Row className="py-0">
+          <Col lg="2">
+            <input
+              type="checkbox"
+              checked={this.state.skills[skillProf]}
+              onChange={event => {
+                this.setState({
+                  skills: {
+                    ...this.state.skills,
+                    [skillProf]: !this.state.skills[skillProf]
+                  }
+                });
+              }}
+              aria-label="Checkbox"
+              aria-describedby={relatedStat}
+            />
+          </Col>
+          <Col lg="2">
+            <input
+              type="checkbox"
+              checked={this.state.skills[skillExp]}
+              onChange={event => {
+                this.setState({
+                  skills: {
+                    ...this.state.skills,
+                    [skillExp]: !this.state.skills[skillExp]
+                  }
+                });
+              }}
+              aria-label="Checkbox"
+              aria-describedby={relatedStat}
+            />
+          </Col>
+          <Col lg="8 text-right">
+            <h5>+2</h5>
+          </Col>
+        </Row>
+      </ListGroup.Item>
+    );
   }
 
   render() {
@@ -111,7 +273,7 @@ class App extends Component {
             </Row>
             <Row>
               <Col lg="4">
-                <InputGroup className="mx-auto" style={{ width: "8em" }}>
+                <InputGroup className="mx-auto input--small">
                   <InputGroup.Prepend>
                     <InputGroup.Text>Level</InputGroup.Text>
                   </InputGroup.Prepend>
@@ -159,8 +321,44 @@ class App extends Component {
           </Col>
         </Row>
         <Row>
-          <Col>
-            <Test data="tits" otherData="poop" />
+          <Col className="text-center">
+            <CardGroup>
+              {this.renderStatCards("Strength")}
+              {this.renderStatCards("Dexterity")}
+              {this.renderStatCards("Constitution")}
+              {this.renderStatCards("Intelligence")}
+              {this.renderStatCards("Wisdom")}
+              {this.renderStatCards("Charisma")}
+            </CardGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="3">
+            <ListGroup>
+              {this.renderSkillCards("Acrobatics", "Dex")}
+              {this.renderSkillCards("Animal Handling", "Wis")}
+              {this.renderSkillCards("Arcana", "Int")}
+              {this.renderSkillCards("Athletics", "Str")}
+              {this.renderSkillCards("Deception", "Cha")}
+              {this.renderSkillCards("History", "Int")}
+              {this.renderSkillCards("Insight", "Wis")}
+              {this.renderSkillCards("Intimidation", "Cha")}
+              {this.renderSkillCards("Investigation", "Int")}
+              {this.renderSkillCards("Medicine", "Wis")}
+              {this.renderSkillCards("Nature", "Int")}
+              {this.renderSkillCards("Perception", "Wis")}
+              {this.renderSkillCards("Performance", "Cha")}
+              {this.renderSkillCards("Persuasion", "Cha")}
+              {this.renderSkillCards("Religion", "Int")}
+              {this.renderSkillCards("Sleight of Hand", "Dex")}
+              {this.renderSkillCards("Stealth", "Dex")}
+              {this.renderSkillCards("Survival", "Wis")}
+            </ListGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center">
+            <ListGroup />
           </Col>
         </Row>
       </Container>
